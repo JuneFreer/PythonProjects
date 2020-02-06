6664bd36a84c6b4a90a91909cd5cd1faaa41460d
Jun Chen <chjun2016@gmail.com>
2020-02-07 05:31:39 +1100

    Using Github API to access data, and using Pygal to visualize data

---
+++ UsingGithubAPI/python_repos.py
@@ -0,0 +1,39 @@
+import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
+# 执行API调用并存储响应
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print("Status code:", r.status_code)
+
+# 将API响应存储在一个变量中, 是一个JSON格式的文件
+response_dict = r.json()
+print("Total repositories:", response_dict['total_count']) # GitHub总共包含多少个Python仓库
+
+# 探索有关仓库的信息
+repo_dicts = response_dict['items']
+print("Repositories returned:", len(repo_dicts)) # 获悉我们获得了多少个仓库的信息
+
+# 研究第一个仓库
+repo_dict = repo_dicts[0] # 提取repo_dicts中的第一个字典（第一个仓库）
+print("\nKeys:", len(repo_dict)) # 看看第一个仓库中有多少信息
+# for key in sorted(repo_dict.keys()): # 我们打印这个字典的所有键，看看其中包含哪些信息
+#     print(key)
+
+names, stars = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# 可视化
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False) # Bar()条形图
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
+
+# 处理结果
+print(response_dict.keys())
